b4910041c908397ee371be3d7e500e2a9a3184c6
Gage Eveland <42362153+EvelAndG@users.noreply.github.com>
2021-10-18 17:08:32 +0000

    Part 2 progress

--- main.sql
+++ main.sql
@@ -3,17 +3,55 @@
 
 --Part 1
 CREATE TABLE cardDecks (
-    card_name varchar(100),
+    card_name varchar(255),
     brand varchar(255),
     total_cards int,    
     price int
 );
 
+CREATE TABLE wishlist (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name varchar(255),
+	quantity varchar(255),
+	price int
+
+
+
+);
+
+
+
+
+
+
 INSERT INTO cardDecks (card_name, brand, total_cards, price)
-VALUES ('Magic Carpet', 'Bicycle', 54, 5), 
-('Monarch', 'Theory 11', 56, 12)
+VALUES ('Standard', 'Bicycle', 54, 7),
+('Magic Carpet', 'Bicycle', 54, 9), 
+('Monarch', 'Theory 11', 56, 12),
+('007 Edition', 'Theory 11', 56, 10)
 ;
 
+INSERT INTO wishlist (name, quantity, price)
+VALUES ('gold', '100lb', 1765);
+
+
+
+
+
+
+
+
+
+
+.print
+.print      ---------------Table 1---------------     
 SELECT *
 FROM cardDecks;
+.print
+
+.print
+.print      ---------------Table 2---------------     
+SELECT *
+FROM wishlist;
+
 
